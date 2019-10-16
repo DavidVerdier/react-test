1089e733bcf1dcf0bc5e21b295c0cd656a22f9e2
David <contact@discutea.com>
2019-10-16 12:43:11 +0200

    initial commit

---
+++ src/components/Form.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class Form extends React.Component {
+    state = {
+        name: '',
+        quantity: 0
+    };
+
+    render() {
+        return (
+            <div>
+                <h3>Ajout des articles à acheter</h3>
+                <form>
+                    <input type="number" placeholder="quantité"
+                           value={this.state.quantity} onChange={(event) => this.setState({quantity: event.target.value})} />
+                    <input type="text" placeholder="article"
+                           value={this.state.name} onChange={(event) => this.setState({name: event.target.value})} />
+                    <button type="submit">Ajouter</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Form;
