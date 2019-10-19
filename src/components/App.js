892125e2fc48546238304e7a117c29fb9a6c31f5
David <contact@discutea.com>
2019-10-19 10:12:46 +0200

    initialize chat-box

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,11 @@
 import React from "react";
-import Form from './Form';
-import ItemList from './ItemList';
 
 
 class App extends React.Component {
-    state = {articles : []};
-
-    addArticle = (article) => {
-        let oldArticles = this.state.articles;
-
-        article.id = Date.now();
-        let newArticles = [...oldArticles, article];
-        this.setState({articles: newArticles});
-    };
-
     render() {
         return (
             <div>
-                <h3>Liste de courses</h3>
-
-                <Form formTitle="Ajout des articles à acheter" addArticle={this.addArticle}/>
-                <ItemList articles={this.state.articles}/>
+                <h3>Ready</h3>
             </div>
         );
     }
