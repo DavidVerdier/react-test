8f27c9db52c8dd688906eb7c71201b3b2985db53
David <contact@discutea.com>
2019-10-19 11:47:05 +0200

    add state and redirect on connection component

--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -1,11 +1,37 @@
 import React, {Component} from 'react';
+import { Redirect } from 'react-router-dom';
 
 class Connexion extends Component {
+    state = {
+        pseudo: '',
+        goToChat: false
+    };
+
+    handleChange = (event) => {
+        const pseudo = event.target.value;
+        this.setState({pseudo});
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.setState({goToChat : true});
+        console.log(this.state);
+    };
+
     render() {
+        if (this.state.goToChat) {
+            return <Redirect to={`/pseudo/${this.state.pseudo}`}></Redirect>
+        }
+
         return (
             <div className="connexionBox">
-                <form className="connexion">
+                <form
+                    onSubmit={this.handleSubmit}
+                    className="connexion"
+                >
                     <input
+                        value={this.state.pseudo}
+                        onChange={this.handleChange}
                         type="text"
                         placeholder="pseudo"
                         required
