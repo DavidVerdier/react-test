3cb9eeef6fcf5a02765c3418c632c4cbbdc03404
David <contact@discutea.com>
2019-10-19 10:31:40 +0200

    add base css

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-
+import './App.css';
 
 class App extends React.Component {
     render() {
         return (
-            <div>
-                <h3>Ready</h3>
-            </div>
+            <div className="box" />
         );
     }
 }
