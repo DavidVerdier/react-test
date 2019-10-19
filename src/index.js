812c3422ec88a9cafd2ba923bcd9dea13cfbc3fe
David <contact@discutea.com>
2019-10-19 10:50:40 +0200

    add router

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './components/App.css';
+import App from "./components/App";
 import Connexion from "./components/Connexion";
 
-ReactDOM.render(<Connexion />, document.getElementById('root'));
+import { BrowserRouter, Route, Switch} from "react-router-dom";
+import NotFound from "./components/NotFound";
+
+const Root = () => (
+  <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={Connexion} />
+        <Route path='/pseudo/:pseudo' component={App} />
+        <Route component={NotFound} />
+      </Switch>
+  </BrowserRouter>
+);
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 
