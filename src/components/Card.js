d244b3ad54851326212ced7429a5a1bb3a299666
David <contact@discutea.com>
2019-10-21 14:33:41 +0200

    try image

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,10 +9,18 @@ const Card = ({ details }) => {
         item => <li key={item}>{item}</li>
     );
 
+    const requireImage = (chemin) => {
+      try {
+          return require(`../img/${chemin}`);
+      }  catch (e) {
+          return require('../img/default.jpeg');
+      }
+    };
+
     return (
         <div className="card">
             <div className="image">
-                <img src={require(`../img/${details.image}`)} alt={details.nom}/>
+                <img src={requireImage(details.image)} alt={details.nom}/>
             </div>
 
             <div className="recette">
